e9d2fe26e7ab779d5b5748dcae1ad11c715edeaf
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-12-12 17:08:49 +0500

    download image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::fs::File;
+use std::io::copy;
+use tempfile::Builder;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp_bin");
+
+        println!("Name of the file to download: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("will be located under: '{:?}'", fname);
+
+        File::create(fname)?
+    };
+
+    let content = response.text().await?;
+    copy(&mut content.as_bytes(), &mut dest);
+
+    Ok(())
 }
